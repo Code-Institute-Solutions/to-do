29820f70e7d17250012036e930574c5609e8fbb6
nakitamccool <nakita@codeinstitute.net>
2017-09-04 12:31:01 +0000

    User can navigate using buttons and hyperlinks
    Added buttons to tasks and taskdetail pages. Updated hyperlink on homepage.

--- to_do_prj/tasks/urls.py
+++ to_do_prj/tasks/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from tasks.views import get_tasks, add_task
+from tasks.views import get_tasks, task_detail, add_task, edit_task
 
 urlpatterns = [
-    url(r'^$', get_tasks),
-    url(r'^add$', add_task)
+    url(r'^$', get_tasks, name='get_tasks'),
+    url(r'^add$', add_task),
+    url(r'^(?P<id>\d+)/edit$', edit_task, name='edit_task'),
+    url(r'^(?P<id>\d+)', task_detail, name='task_detail')
 ]
--- to_do_prj/tasks/views.py
+++ to_do_prj/tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
@@ -7,16 +8,32 @@ def index(request):
     
 def get_tasks(request):
     return render(request, 'tasks.html', {'task_list': Task.objects.all()})
-    
+ 
+def task_detail(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.save()
+    return render(request, "taskdetail.html", {'task': task})
+
 def add_task(request):
     if request.method == "POST":
-        new = Task()
-        new.name = request.POST.get('name')
-        new.description = request.POST.get('description')
-        new.status = request.POST.get('status')
-
-        new.save()
-
-        return render(request, 'tasks.html', {'task_list': Task.objects.all()})
+        form = TaskForm(request.POST, request.FILES)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect(task_detail, task.pk)
     else:
-        return render(request, 'add_task.html')
+        form = TaskForm()
+    return render(request, 'taskform.html', {'form': form})
+
+
+def edit_task(request, id):
+   task = get_object_or_404(Task, pk=id)
+   if request.method == "POST":
+       form = TaskForm(request.POST, request.FILES, instance=task)
+       if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect(task_detail, task.pk)
+   else:
+       form = TaskForm(instance=task)
+   return render(request, 'taskform.html', {'form': form})
