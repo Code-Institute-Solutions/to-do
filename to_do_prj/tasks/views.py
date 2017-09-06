bae41220f381314d69718bf6aa778ea3ee7a3bc7
Yoni Lavi <yoni@codeinstitute.net>
2017-09-06 02:10:07 +0100

    standardise docstrings and minor simplifications

--- to_do_prj/tasks/views.py
+++ to_do_prj/tasks/views.py
@@ -4,21 +4,23 @@ from .forms import TaskForm
 
 
 def get_tasks(request):
-    return render(request, 'tasks.html', {'task_list': Task.objects.all()})
+    """A view that displays all tasks"""
+    all_tasks = Task.objects.all()
+    return render(request, 'tasks.html', {'task_list': all_tasks})
 
 
 def task_detail(request, id):
+    """A view that displays the details of a specific task"""
     task = get_object_or_404(Task, pk=id)
     return render(request, "taskdetail.html", {'task': task})
 
 
 def add_task(request):
-    # A view that handles the form for adding a new task
+    """A view that handles the form for adding a new task"""
     if request.method == "POST":
         form = TaskForm(request.POST, request.FILES)
         if form.is_valid():
-            task = form.save(commit=False)
-            task.save()
+            task = form.save()
             return redirect(task_detail, task.pk)
     else:
         form = TaskForm()
@@ -27,10 +29,10 @@ def add_task(request):
 
 
 def edit_task(request, id):
-    # A view that handles the form for editing an existing task
+    """A view that handles the form for editing an existing task"""
     task = get_object_or_404(Task, pk=id)
     if request.method == "POST":
-        form = TaskForm(request.POST, request.FILES, instance=task)
+        form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             task.save()
             return redirect(task_detail, task.pk)
@@ -40,8 +42,8 @@ def edit_task(request, id):
 
 
 def toggle_status(request, id):
-    # A view that handles the form for setting a task as Done
+    """A view that toggles the value of a task's Done field"""
     task = get_object_or_404(Task, pk=id)
     task.done = not task.done
     task.save()
-    return redirect(reverse('get_tasks'))
+    return redirect(get_tasks)
